smx-smx/clover

commit f522fe00ac8e9c74744965b9cd2384ed355ea470

yosemite adaptation
git-svn-id: svn://svn.code.sf.net/p/cloverefiboot/code@2691 52d7a1c0-eba5-4e63-a9d7-e3186cd7c462

rEFIt_UEFI/Platform/Settings.c
@@ -4426,6 +4426,9 @@ CHAR8 *GetOSVersion(IN LOADER_ENTRY *Entry)
           if (AsciiStrStr(prop->string, "Install%20OS%20X%20Mavericks.app")) {
             OSVersion = AllocateZeroPool(5);
             UnicodeStrToAsciiStr(L"10.9", OSVersion);
+          } else if (AsciiStrStr(prop->string, "Install%20OS%20X%20Yosemite.app")) { //xxx
+            OSVersion = AllocateZeroPool(6);
+            UnicodeStrToAsciiStr(L"10.10", OSVersion);
           } else if (AsciiStrStr(prop->string, "Install%20OS%20X%20Mountain%20Lion.app")) {
             OSVersion = AllocateZeroPool(5);
             UnicodeStrToAsciiStr(L"10.8", OSVersion);
@@ -4485,6 +4488,9 @@ CHAR16 *GetOSIconName(IN CHAR8 *OSVersion)
   } else if (AsciiStrStr(OSVersion, "10.9") != 0) {
     // Mavericks
     OSIconName = L"mav,mac";
+  } else if (AsciiStrStr(OSVersion, "10.10") != 0) {
+    // Yosemite
+    OSIconName = L"yos,mac";
   } else {
     OSIconName = L"mac";
   }
@@ -4948,8 +4954,8 @@ CHAR16* GetExtraKextsDir(CHAR8 *OSVersion)
   CHAR8   FixedVersion[6] = "Other";
 
   if (OSVersion != NULL) {
-    AsciiStrnCpy(FixedVersion, OSVersion, 4);
-    FixedVersion[4] = 0;
+    AsciiStrnCpy(FixedVersion, OSVersion, AsciiStrLen(OSVersion));
+    FixedVersion[AsciiStrLen(OSVersion)] = 0;
   }
   
   //MsgLog("OS=%s\n", OSTypeStr);

CloverPkg.dec
@@ -84,7 +84,7 @@
 #
 ################################################################################
 [PcdsFixedAtBuild]
-  gEfiMdeModulePkgTokenSpaceGuid.PcdSmbiosVersion|0x0206|UINT32|0x10010001
+  gEfiMdeModulePkgTokenSpaceGuid.PcdSmbiosVersion|0x0204|UINT32|0x10010001
   gEfiMdeModulePkgTokenSpaceGuid.PcdHelloWorldPrintString|L"UEFI Hello Clover!\n"|VOID*|0x10010002
   gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVendor|L"CLOVER"|VOID*|0x10010003
   gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareRevision|0x00010010|UINT32|0x10010004

rEFIt_UEFI/Platform/kext_inject.c
@@ -239,9 +239,9 @@ EFI_STATUS LoadKexts(IN LOADER_ENTRY *Entry)
 	} else if (Arch != NULL && StrnCmp(Arch,L"i386",StrLen(L"i386")) == 0) {
 		archCpuType = CPU_TYPE_I386;
 	} else if (Entry->OSVersion != NULL && AsciiStrnCmp(Entry->OSVersion,"10.",3) == 0) {
-		if (Entry->OSVersion[3] >= '8') {
+		if ((Entry->OSVersion[3] >= '8') || (Entry->OSVersion[3] == '1')) {  //assume 10.10 - 10.19
 			archCpuType = CPU_TYPE_X86_64; // For >= 10.8, only x86_64 exists
-		} else if (Entry->OSVersion[3] >= '0' && Entry->OSVersion[3] <= '6') {
+		} else if (Entry->OSVersion[3] >= '3' && Entry->OSVersion[3] <= '6') {
 			archCpuType = CPU_TYPE_I386; // For <= 10.6, use default of i386
 		}
 	}

rEFIt_UEFI/entry_scan/loader.c
@@ -672,7 +672,9 @@ STATIC VOID AddDefaultMenu(IN LOADER_ENTRY *Entry)
   }
 
   // Only kernels up to 10.7 have 32-bit mode
-  KernelIs64BitOnly = (Entry->OSVersion == NULL || AsciiStrnCmp(Entry->OSVersion,"10.",3) != 0 || Entry->OSVersion[3] > '7');
+  KernelIs64BitOnly = (Entry->OSVersion == NULL ||
+                       (Entry->OSVersion[3] > '7') ||
+                       (Entry->OSVersion[3] == '1'));
   
   FileName = Basename(Entry->LoaderPath);
   

commit 1206325e4f8294ab4055e0cbbf190f6bc9a28796

Correct mistake with OSVersion introduced in r2691
git-svn-id: svn://svn.code.sf.net/p/cloverefiboot/code@2694 52d7a1c0-eba5-4e63-a9d7-e3186cd7c462

rEFIt_UEFI/Platform/Settings.c
@@ -4952,10 +4952,17 @@ CHAR16* GetExtraKextsDir(CHAR8 *OSVersion)
 {
   CHAR16 *SrcDir = NULL;
   CHAR8   FixedVersion[6] = "Other";
+  CHAR8  *DotPtr;
 
   if (OSVersion != NULL) {
-    AsciiStrnCpy(FixedVersion, OSVersion, AsciiStrLen(OSVersion));
-    FixedVersion[AsciiStrLen(OSVersion)] = 0;
+    AsciiStrnCpy(FixedVersion, OSVersion, 5);
+    // OSVersion may contain minor version too (can be 10.x or 10.x.y)
+    if ((DotPtr = AsciiStrStr(FixedVersion, ".")) != NULL) {
+      DotPtr = AsciiStrStr(DotPtr+1, "."); // second dot
+    }
+    if (DotPtr != NULL) {
+      *DotPtr = 0;
+    }
   }
   
   //MsgLog("OS=%s\n", OSTypeStr);